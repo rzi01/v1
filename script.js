c37678105e04f66bd5811ac116a7ed025e97add8
rafiu <rzithunder00@gmail.com>
2024-02-14 10:06:54 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function askValentine(choice) {
+    if (choice === 'yes') {
+      window.location.href = 'congratulations.html';
+    } else if (choice === 'no') {
+      window.location.href = 'index.html';
+    }
+  }
+  
